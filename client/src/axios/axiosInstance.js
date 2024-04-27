cb3a383e6a96d2ddd81516c24c0335b322101bea
Diwash Karki <diwaskarki96@gmail.com>
2024-04-27 19:06:42 +0545

    minor change

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const $axios = axios.create({
-  baseURL: "https://registration-form-api.vercel.app",
+  baseURL: "https://registration-form-api.onrender.com",
   timeout: 5000,
 });
